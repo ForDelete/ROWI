e0e9eb94c70b4c5c65ec2fc48d027daab5a2bcdd
ForDelete <105589004+ForDelete@users.noreply.github.com>
2023-05-23 22:00:33 +0400

    plus front

---
+++ clubsite/src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import {useState} from "react";
+
+function Footer() {
+    const [count, setCount] = useState(0)
+
+    return (<div>Footer</div>)
+}
+export default Footer
---
+++ clubsite/src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import {useState} from "react";
+
+function Header() {
+    const [count, setCount] = useState(0)
+
+    return (<div>Header</div>)
+}
+export default Header
